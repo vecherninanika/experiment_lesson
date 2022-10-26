75050dc6a9d82b5c92dc1fdaedf99cacac417ea9
annashutova <annashut@inbox.ru>
2022-10-26 14:30:46 +0300

    Добавила декоратор для функций

--- count_duplicates.py
+++ count_duplicates.py
@@ -1,3 +1,17 @@
+import time
+
+
+def decorator(f1, f2):
+    def new_counter(string):
+        initial_time = time.time()
+        f1(string)
+        f1_time = time.time() - initial_time
+        initial_time = time.time()
+        f2(string)
+        f2_time = time.time() - initial_time
+        print(f1.__name__ if f1_time < f2_time else f2.__name__)
+    return new_counter
+
 def count_duplicates(s):
     return len([ch for ch in (set(s.lower())) if s.lower().count(ch) > 1])
 
@@ -14,3 +28,6 @@ def letters(string):
         else:
             list_1.append(letter)
     return res
+
+function = decorator(count_duplicates, letters)
+function('aaaBBB')
